1869a612bdaf46efd8474dcd8f9b7d92f060be3b
Adriana Ito <ito.dri@gmail.com>
2022-02-07 17:00:33 +0900

    exports a copy of the contract that we can work with from React side

---
+++ src/lottery.js
@@ -0,0 +1,58 @@
+import web3 from "./web3";
+
+const address = "0x34413B40BB9C993D5D151f2007F09373c9C59e20";
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+// exports a complete copy of the contract that we can work with from the React side.
+export default new web3.eth.Contract(abi, address);
